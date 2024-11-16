867181ec502b0711ff08efdf214a37a0881223ba
intel__gence <palosibasis2000@outlook.com>
2024-11-16 20:08:52 +0530

    Added an event handler for switching internal and external colors

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,10 @@
 const topBar = document.getElementById('top-bar');
-const atTop = window.scrollY === 0;
+const exteriorColorSection = document.getElementById('exterior-buttons');
+const interiorColorSection = document.getElementById('interior-buttons');
+const extImage = document.querySelector('#exterior-image');
+const intImage = document.querySelector('#interior-image');
+
+
 
 // Handle Top Bar on Scroll
 const handleScroll = () => {
@@ -8,5 +13,23 @@ const handleScroll = () => {
     topBar.classList.toggle('hidden-bar', !atTop);
 };
 
+//Handle Color Sections
+const handleColorButtonClick = (event)=>{
+    let button;
+    if (event.target.tagName == 'IMG'){
+        button = event.target.closest('button');
+    }
+    else if(event.target.tagName == 'BUTTON'){
+        button = event.target;
+    }
+    if(button){
+        const buttons = event.currentTarget.querySelectorAll('button');
+        buttons.forEach((btn)=> btn.classList.remove('btn-selected'));
+        button.classList.add('btn-selected');
+    }
+}
+
 // Event Listener for Scrolling
 window.addEventListener('scroll', () => requestAnimationFrame(handleScroll)); //requestAnimationFrame is used to handle performance else handleScroll will be called multiple times
+exteriorColorSection.addEventListener('click', handleColorButtonClick);
+interiorColorSection.addEventListener('click', handleColorButtonClick);
